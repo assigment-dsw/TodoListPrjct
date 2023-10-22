ebacab5586f382bb849206bd651a51eb64da6016
SebaRosalesZ <130470153+SebaRosalesZ@users.noreply.github.com>
2023-10-21 23:56:57 -0400

    Add files via upload

---
+++ Controllers/TodoItemController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+using TodoList.Services;
+
+namespace TodoList.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class TodoItemController : Controller
+    {
+        private readonly MongoDBService _mongoDBService;
+
+        public TodoItemController(MongoDBService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        [HttpGet]
+        public async Task<List<TodoItem>> Get()
+        {
+            return await _mongoDBService.GetAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TodoItem todoItem)
+        {
+            await _mongoDBService.CreateAsync(todoItem);
+            return Ok(todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, [FromBody] TodoItem todoItem)
+        {
+            var existingItem = await _mongoDBService.GetAsyncById(id);
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            await _mongoDBService.UpdateAsync(id, todoItem);
+            return Ok(todoItem);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var existingItem = await _mongoDBService.GetAsyncById(id);
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            await _mongoDBService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+
+}
+
---
+++ Services/MongoDBService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using TodoList.Models;
+
+namespace TodoList.Services
+{
+	public class MongoDBService
+	{
+        private readonly IMongoCollection<TodoItem> _todoItemCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _todoItemCollection = database.GetCollection<TodoItem>(mongoDBSettings.Value.CollectionName);
+        }
+
+        public async Task<List<TodoItem>> GetAsync()
+        {
+            return await _todoItemCollection.Find(item => true).ToListAsync();
+        }
+
+        public async Task CreateAsync(TodoItem todoItem)
+        {
+            await _todoItemCollection.InsertOneAsync(todoItem);
+        }
+
+        public async Task UpdateAsync(string id, TodoItem todoItem)
+        {
+            var filter = Builders<TodoItem>.Filter.Eq(item => item.Id, id);
+            await _todoItemCollection.ReplaceOneAsync(filter, todoItem);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var filter = Builders<TodoItem>.Filter.Eq(item => item.Id, id);
+            await _todoItemCollection.DeleteOneAsync(filter);
+        }
+
+        public async Task<TodoItem> GetAsyncById(string id)
+        {
+            var filter = Builders<TodoItem>.Filter.Eq(item => item.Id, id);
+            return await _todoItemCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+      
+    }
+}
+
